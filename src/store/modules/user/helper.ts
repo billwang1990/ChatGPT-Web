221a15f925e8df01ecb3efff7776afdbfd35da9b
GoForIt <billwang1990@gmail.com>
2023-04-10 15:51:34 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/poloLogo.jpg',
+      name: '保罗yyds',
+      description: `Contact Me <a href="#" onclick="alert(请加wx:bllh888') class="text-blue-500" target="_blank" >wx: bllh888</a>`,
     },
   }
 }
